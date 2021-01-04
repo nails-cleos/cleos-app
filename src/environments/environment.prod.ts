da0d5801961a3d94fbe6ddbf84a77b198640e6d6
Lucas Scarlatta <lucasscarlatta@gmail.com>
2021-01-04 17:28:06 -0300

    Add i18n

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'http://localhost:8080/api',
+  googleClientId: '101919845477-fdnq6upj7rcuoepia39nom2vattg27nu.apps.googleusercontent.com',
+  facebookClientId: '453309609006101'
 };
