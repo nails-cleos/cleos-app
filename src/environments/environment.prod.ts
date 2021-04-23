96ef850faba05cf8fb672f9e7390043c781a36b6
Lucas Scarlatta <lucasscarlatta@gmail.com>
2021-04-23 15:32:06 -0300

    Add docker

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://192.168.0.23:8080/api',
-  wsEndpoint: 'http://192.168.0.23:8080/api/ws',
+  baseUrl: 'http://192.168.0.23/api',
+  wsEndpoint: 'http://192.168.0.23/api/ws',
   googleClientId: '57757755712-1gcjdgbd0lkj4msirrmra9aglcb0vbmk.apps.googleusercontent.com',
   facebookClientId: '453309609006101',
   googleMapKey: 'AIzaSyBVYE6eWD4Ekmb4xdWz_N4QKk5p4Q8oIcc'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:8080/api',
-  wsEndpoint: 'http://localhost:8080/api/ws',
+  baseUrl: 'http://localhost/api',
+  wsEndpoint: 'http://localhost/api/ws',
   googleClientId: '57757755712-1gcjdgbd0lkj4msirrmra9aglcb0vbmk.apps.googleusercontent.com',
   facebookClientId: '453309609006101',
   googleMapKey: 'AIzaSyBVYE6eWD4Ekmb4xdWz_N4QKk5p4Q8oIcc'
