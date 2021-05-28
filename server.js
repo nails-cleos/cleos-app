40e15a804b5e882a8cfee9ed4a34e0b0b8db1343
Lucas Scarlatta <lucasscarlatta@gmail.com>
2021-05-28 15:42:08 -0300

    Heroku deploy (#7)
    * Update environment.prod.ts
    
    * updates to deploy to heroku
    
    * updates to deploy to heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files
+app.use(express.static(__dirname + '/dist/cleos-app'));
+
+// Send all requests to index.html
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/cleos-app/index.html'));
+});
+
+// default Heroku port
+app.listen(process.env.PORT || 5000);
