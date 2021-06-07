637cf1d98a425955e9bf55d4f246b6c6bff1c964
Lucas Scarlatta <lucasscarlatta@gmail.com>
2021-06-07 12:51:02 -0300

    Update environment staging

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   title: 'CLEOS STAGING',
-  baseUrl: 'http://localhost:8080/api',
+  baseUrl: 'https://cleos-staging.herokuapp.com/api',
   googleClientId: '57757755712-1gcjdgbd0lkj4msirrmra9aglcb0vbmk.apps.googleusercontent.com',
   facebookClientId: '453309609006101',
   firebase: {
