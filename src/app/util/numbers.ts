5f60da304e39270b1aeab51cf874ee78aa8085d1
Lucas Scarlatta <lucas.scarlatta@vayapay.com>
2023-05-20 14:13:44 +0200

    Update dashboard add numbers convertion

---
+++ src/app/util/numbers.ts
@@ -0,0 +1,13 @@
+import { API_LOCALE } from './dates';
+import { ICurrency } from '../interfaces/currency';
+import { currencySymbol } from './helper';
+
+export const numberFormat = (value: number | string, currency?: ICurrency, locale: string = API_LOCALE) => {
+  const options = currency ? { maximumFractionDigits: 2, currency: currency.code, style: 'currency' } : { maximumFractionDigits: 2 };
+
+  if (typeof value === 'string') {
+    return currency ? `${currencySymbol(currency)}${value}` : value;
+  }
+
+  return new Intl.NumberFormat(locale, options).format(Number(value));
+};
